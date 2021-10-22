5cc3c6d6171403aae2fb90a6b14fdbb0727b912e
Augusto Dantas <aldantas@protonmail.com>
2021-10-21 21:33:16 -0300

    Fix BooleanSolution

--- hhrl/solution/boolean_solution.py
+++ hhrl/solution/boolean_solution.py
@@ -2,7 +2,8 @@ from hhrl.solution import Solution
 
 
 class BooleanSolution(Solution):
-    def __init__(self, bool_tuple):
+    def __init__(self, id=0, bool_tuple=tuple(), fitness=float('inf')):
+        Solution.__init__(self, id, fitness)
         self.bool_tuple = bool_tuple
 
     def __str__(self):
@@ -10,4 +11,4 @@ class BooleanSolution(Solution):
 
     def distance(self, other):
         diff = [a^b for a,b in zip(self.bool_tuple, other.bool_tuple)]
-        return sum(diff)/len(self.bool_list)
+        return sum(diff)/len(self.bool_tuple)
