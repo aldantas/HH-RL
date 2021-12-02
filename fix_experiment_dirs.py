b7301b98f9dcfc6798c083aae3b8478aeb4f139d
Augusto Dantas <aldantas@protonmail.com>
2021-12-02 18:28:18 -0300

    Fix wrong experiemnt paths

---
+++ fix_experiment_dirs.py
@@ -0,0 +1,15 @@
+import os
+from hhrl.util import Loader
+
+l = Loader()
+l.check_experiments('/home/aldantas/nas/aldantas/results_data_HHRL_states/')
+
+# for line in open('temp.txt'):
+#     # cur_dir = '/home/aldantas/github/HH-RL'
+#     parts = line.rstrip('\n').split('/')[:-3]
+#     input_dir = '/'.join(parts)
+#     parts[-1] = 'SW'
+#     output_dir = '/'.join(parts)
+#     print(input_dir)
+#     print(output_dir)
+#     os.system(f'mv {input_dir} {output_dir}')
